913c291a3a7517b356b991b714b37fd7fb620d82
François GERBES <francois.gerbes@libre-informatique.fr>
2018-03-27 09:46:57 +0200

    [CI] Disable postgres & enable selenium

--- Scripts/create.local.docker.env.sh
+++ Scripts/create.local.docker.env.sh
@@ -9,10 +9,10 @@ export Version=3.3.1
 export Hostname=etcd.host
 run_pipeline_sh Jenkins/etcd.pipeline
 
-export Hostname=postgres.host
-export Version=9.4
-export Password=postgres24
-run_pipeline_sh Jenkins/postgres.pipeline
+#export Hostname=postgres.host
+#export Version=9.4
+#export Password=postgres24
+#run_pipeline_sh Jenkins/postgres.pipeline
 
 export Version=0.15.0
 export Hostname=confd.host
@@ -21,3 +21,7 @@ run_pipeline_sh Jenkins/confd.pipeline
 export Version=6.1.1
 export Hostname=elastic.host
 run_pipeline_sh Jenkins/elastic.pipeline
+
+export Version=firefox:3.7.0-arsenic
+export Hostname=selenium.host
+run_pipeline_sh Jenkins/selenium.pipeline
