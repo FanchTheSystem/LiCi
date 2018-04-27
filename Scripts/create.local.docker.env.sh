6474d53d017500986470fce9627a22c3e45772e7
François GERBES <francois.gerbes@libre-informatique.fr>
2018-04-27 11:29:24 +0200

    [CI] create local env more explicit

--- Scripts/create.local.docker.env.sh
+++ Scripts/create.local.docker.env.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
-source Scripts/pipeline.usefull.bash.function
-
-run_pipeline_sh Jenkins/docker.clean.pipeline
-
-export Version=3.3.1
-export Hostname=etcd.host
-run_pipeline_sh Jenkins/etcd.pipeline
-
-#export Hostname=postgres.host
-#export Version=9.4
-#export Password=postgres24
-#run_pipeline_sh Jenkins/postgres.pipeline
-
-export Version=0.15.0
-export Hostname=confd.host
-run_pipeline_sh Jenkins/confd.pipeline
-
-export Version=6.1.1
-export Hostname=elastic.host
-run_pipeline_sh Jenkins/elastic.pipeline
-
-#export Version=firefox:3.7.0-arsenic
-#export Hostname=selenium.host
-#run_pipeline_sh Jenkins/selenium.pipeline
+for i in $(docker container ls -q); do docker container stop $i; docker container rm -v $i; done
+docker network prune -f
+docker system prune -a -f
+docker volume prune -f
+docker network create ci.network
+docker images
+docker container ls
+docker network ls --no-trunc
+docker network inspect ci.network
+docker container stop etcd.host || echo ok
+docker container rm etcd.host || echo ok
+docker build --build-arg ETCDVER=3.3.1 --build-arg UID=`id -u` --build-arg GID=100 -t etcd-img-3.3.1 -f Docker/etcd.dockerfile .
+docker run -dt -p 2379:2379 --network=ci.network --name etcd.host etcd-img-3.3.1
+docker exec etcd.host etcd --version
+docker container stop confd.host || echo ok
+docker container rm confd.host || echo ok
+docker build --build-arg CONFDVER=0.15.0 --build-arg UID=`id -u` --build-arg GID=100 -t confd-img-0.15.0 -f Docker/confd.dockerfile .
+docker run -dt --network=ci.network --volume /home/francois/public_html:/home/jenkins/public_html --name confd.host confd-img-0.15.0
+docker exec confd.host confd --version
